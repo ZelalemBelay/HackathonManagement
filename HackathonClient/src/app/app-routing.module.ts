e7fac3df0c1537f1576dd5cd1c7e7eaa4fa8a479
zelalem <zelalem.belay1@gmail.com>
2019-06-13 16:26:29 -0500

    Done with routing

--- HackathonClient/src/app/app-routing.module.ts
+++ HackathonClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+{ path: 'signUp', component: AppComponent },
+{ path: 'login', component: AppComponent },
+// { path: '**', component: AppComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- HackathonClient/src/app/app.module.ts
+++ HackathonClient/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
