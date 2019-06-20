43ea3ea0b5167ab630f209d15b9b431ea9e8ec10
Mariamawit <mawitey34@gmail.com>
2019-06-20 15:49:31 -0500

    added routing module

--- HackathonClient/src/app/app-routing.module.ts
+++ HackathonClient/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TeamSignUpComponent } from './team-sign-up/team-sign-up.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { EvaluationFormComponent } from './evaluation-form/evaluation-form.component';
 
 const routes: Routes = [  
 { path: 'signUp', component: TeamSignUpComponent },
+{ path: 'evaluation', component: EvaluationFormComponent },
 { path: 'login', component: AppComponent },
 { path: '**', component: HomepageComponent }
 ];
