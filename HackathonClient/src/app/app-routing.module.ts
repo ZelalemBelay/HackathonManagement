0287803209fa66e545dda821fabf5a4681933d23
zelalem <zelalem.belay1@gmail.com>
2019-06-21 18:46:39 -0500

    Merge conflicts

--- HackathonClient/src/app/app-routing.module.ts
+++ HackathonClient/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TeamSignUpComponent } from './team-sign-up/team-sign-up.component';
 import { HomepageComponent } from './homepage/homepage.component';
-
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HackathonHostsComponent } from './admin/hackathon-hosts/hackathon-hosts.component';
